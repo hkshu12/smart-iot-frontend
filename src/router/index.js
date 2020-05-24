cf66a8efd9b44572b7cabc1d63565e234425d362
Jinyao <541639324@qq.com>
2020-05-24 09:04:05 +0800

    add 404 page & title

--- src/router/index.js
+++ src/router/index.js
@@ -13,59 +13,94 @@ const routes = [
     path: '/Summary',
     name: '数据概览',
     component: Summary,
-    title: 'Summary',
+    meta: {
+      title: '数据概览',
+    },
   },
   {
     path: '/Device',
     name: '设备',
-    component: () => import('@/views/Device/Device.vue'),
+    component: () => import(/* webpackChunkName: "device" */ '@/views/Device/Device.vue'),
     children: [
       {
         path: 'Template',
         name: '设备模板',
-        component: () => import('@/views/Device/Template.vue'),
+        component: () => import(/* webpackChunkName: "device" */ '@/views/Device/Template.vue'),
+        meta: {
+          title: '设备模板',
+        },
       },
       {
         path: 'Management',
         name: '设备管理',
-        component: () => import('@/views/Device/Manage.vue'),
+        component: () => import(/* webpackChunkName: "device" */ '@/views/Device/Manage.vue'),
+        meta: {
+          title: '设备管理',
+        },
       },
     ],
   },
   {
     path: '/Rule',
     name: '规则引擎',
-    component: () => import('@/views/Rule/Rule.vue'),
+    component: () => import(/* webpackChunkName: "rule" */ '@/views/Rule/Rule.vue'),
     children: [
       {
         path: 'DeviceLinkage',
         name: '设备联动',
-        component: () => import('@/views/Rule/DeviceLinkage.vue'),
+        component: () => import(/* webpackChunkName: "rule" */ '@/views/Rule/DeviceLinkage.vue'),
+        meta: {
+          title: '设备联动',
+        },
       },
       {
         path: 'DataForwarding',
         name: '数据转发',
-        component: () => import('@/views/Rule/DataForwarding.vue'),
+        component: () => import(/* webpackChunkName: "rule" */ '@/views/Rule/DataForwarding.vue'),
+        meta: {
+          title: '数据转发',
+        },
       },
     ],
   },
   {
     path: '/DataAnalytics',
     name: '数据分析',
-    component: () => import('@/views/DataAnalytics/DataAnalytics.vue'),
-    title: 'DataAnalytics',
+    component: () => import(/* webpackChunkName: "data" */ '@/views/DataAnalytics/DataAnalytics.vue'),
+    meta: {
+      title: '数据分析',
+    },
   },
   {
     path: '/About',
     name: '关于平台',
     component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue'),
-    title: 'About',
+    meta: {
+      title: '关于平台',
+    },
+  },
+  {
+    path: '*',
+    redirect: '/404',
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('@/views/404.vue'),
+    meta: {
+      title: '404',
+    },
   },
 ];
 
 const router = new VueRouter({
   mode: 'history',
   routes,
+});
+
+router.afterEach((to) => {
+  console.log(to);
+  document.title = `Smart IoT | ${to.meta.title}`;
 });
 
 export default router;
---
+++ src/views/404.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>404 Not Found</p>
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+div
+  height 100%
+  widht 100%
+  position relative
+div > p
+  position absolute
+  top 50%
+  left 50%
+  transform translate(-50%,-50%)
+  font-size 4rem
+</style>
