c106564f07af70ffdfc3f404110cb3a847719efe
Jinyao <541639324@qq.com>
2020-06-18 20:18:32 +0800

    dev: 完成实时分析任务页

--- src/views/DataAnalytics/RealtimeAnalysis.vue
+++ src/views/DataAnalytics/RealtimeAnalysis.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout-content>
-    <a-collapse accordion>
+    <a-collapse accordion defaultActiveKey="model">
       <a-collapse-panel key="model" header="模型">
         <a-table
           :columns="modelColumns"
@@ -59,6 +59,36 @@
         </a-table>
       </a-collapse-panel>
       <a-collapse-panel key="task" header="任务">
+        <a-table
+          :columns="taskColumns"
+          :data-source="taskList"
+          :loading="taskLoading"
+          :row-key="record => record.taskId"
+          :pagination="false"
+        >
+          <template slot="customTitle">
+            <a-button @click="onClickAddTask">添加任务</a-button>
+          </template>
+          <template slot="operation" slot-scope="record">
+            <a-popconfirm
+              placement="topLeft"
+              ok-text="确认"
+              cancel-text="取消"
+              @confirm="onClickDeleteTask(record.taskId)"
+              style="margin-left:14px;">
+              <template slot="title">
+                <p>确认要删除该任务吗？</p>
+              </template>
+              <a-button
+                type="danger"
+              >删除</a-button>
+            </a-popconfirm>
+            <a-button
+              @click="onClickExecuteTask(record.taskId)"
+              style="margin-left:14px;"
+            >执行</a-button>
+          </template>
+        </a-table>
       </a-collapse-panel>
     </a-collapse>
     <a-modal
@@ -176,6 +206,108 @@
       </a-form-model-item>
       </a-form-model>
     </a-modal>
+    <a-modal
+      :visible="taskModalVisible"
+      title="添加模型"
+      @cancel="handleTaskModalCancel"
+      @ok="submitTask"
+    >
+      <a-form-model
+        ref="taskForm"
+        :model="taskForm"
+        :label-col="{ span: 4 }"
+        :wrapper-col="{ span: 18 }"
+      >
+        <a-form-model-item
+        ref="taskName"
+        label="任务名称"
+        prop="taskName"
+      >
+        <a-input
+          v-model="taskForm.name"
+          placeholder="请输入任务名称"
+        ></a-input>
+      </a-form-model-item>
+      <a-form-model-item
+        ref="description"
+        label="任务描述"
+        prop="description"
+      >
+        <a-textarea
+          v-model="taskForm.description"
+          :auto-size="{minRows:3, maxRows:3}"
+          placeholder="请输入任务描述"
+        />
+      </a-form-model-item>
+      <a-form-model-item
+        ref="description"
+        label="数据通道"
+        prop="description"
+      >
+        <a-select
+          v-model="taskForm.channelIds"
+          mode="multiple"
+          placeholder="请选择数据通道"
+          @change="onSelectChannels"
+          >
+          <a-select-option
+            v-for="channel in channelList"
+            :key="channel.id"
+            :value="channel.id">
+            {{channel.channelName}}
+          </a-select-option>
+        </a-select>
+      </a-form-model-item>
+      <a-form-model-item
+          ref="taskModel"
+          label="选择模型"
+          prop="taskModel"
+        >
+          <a-select
+            v-model="taskForm.modelId"
+            placeholder="请选择模型"
+            @change="onSelectTaskModel"
+            >
+            <a-select-option v-for="model in modelList" :key="model.modelId" :value="model.modelId">
+              {{model.name}}
+            </a-select-option>
+          </a-select>
+        </a-form-model-item>
+        <a-form-model-item
+          v-for="input in taskModelInputs"
+          :key="input.fieldId"
+          :ref="`taskInput${input.fieldId}`"
+          :label="input.fieldName"
+          :prop="`taskInput${input.fieldId}`"
+          :label-col="{ span: 6 }"
+          :wrapper-col="{ span: 18 }"
+        >
+           <a-select
+            mode="multiple"
+            placeholder="请选择数据通道中的字段"
+            @change="(value) => onInputFieldChannelFieldChange(value, input.fieldId)"
+            >
+            <a-select-option
+              v-for="field in fieldList"
+              :key="field.id"
+              :value="field.id">
+              {{field.fieldName}}
+            </a-select-option>
+          </a-select>
+          <a-select
+            placeholder="请选择算子"
+            @change="(value) => onInputFieldOperatorChange(value, input.fieldId)"
+            >
+            <a-select-option
+              v-for="operator in operatorList"
+              :key="operator.operatorId"
+              :value="operator.operatorId">
+              {{operator.name}}
+            </a-select-option>
+          </a-select>
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
   </a-layout-content>
 </template>
 
@@ -226,6 +358,29 @@ const operatorColumns = [
   },
 ];
 
+const taskColumns = [
+  {
+    title: '任务ID',
+    dataIndex: 'taskId',
+    key: 'taskId',
+  },
+  {
+    title: '任务名',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: '任务描述',
+    dataIndex: 'description',
+    key: 'description',
+  },
+  {
+    slots: { title: 'customTitle' },
+    key: 'operation',
+    scopedSlots: { customRender: 'operation' },
+  },
+];
+
 export default {
   name: 'RealtimeAnalysis',
   data() {
@@ -244,12 +399,25 @@ export default {
       operatorModalType: 0,
       operatorForm: {
       },
+      taskColumns,
+      taskList: [],
+      taskLoading: false,
+      taskModalVisible: false,
+      taskModalType: 0,
+      taskForm: {
+      },
+      taskModelInputs: [],
+      channelList: [],
+      fieldList: [],
+      inputForm: {},
       uploading: false,
     };
   },
   mounted() {
     this.fetchModel();
     this.fetchOperator();
+    this.fetchTask();
+    this.fetchChannel();
   },
   methods: {
     async fetchModel() {
@@ -264,6 +432,16 @@ export default {
       this.operatorList = res.data;
       this.operatorLoading = false;
     },
+    async fetchTask() {
+      this.taskLoading = true;
+      const res = await this.$axios('/analysis/online/task/all');
+      this.taskList = res.data;
+      this.taskLoading = false;
+    },
+    async fetchChannel() {
+      const res = await this.$axios('/channel/list/device-channel');
+      this.channelList = res;
+    },
     onClickAddModel() {
       this.addModelModalVisible = true;
     },
@@ -276,6 +454,15 @@ export default {
       this.operatorModalType = 1;
       this.operatorForm = { ...record };
     },
+    onClickAddTask() {
+      this.taskModalVisible = true;
+      this.taskModalType = 0;
+    },
+    // onClickUpdateTask(record) {
+    //   this.operatorModalVisible = true;
+    //   this.operatorModalType = 1;
+    //   this.operatorForm = { ...record };
+    // },
     async onClickDeleteModel(id) {
       this.$message.loading({ content: '删除中...', key: id });
       try {
@@ -304,6 +491,34 @@ export default {
         this.$message.error({ content: '删除失败', key: id, duration: 3 });
       }
     },
+    async onClickDeleteTask(id) {
+      this.$message.loading({ content: '删除中...', key: id });
+      try {
+        const res = await this.$axios(`/analysis/online/task/delete?taskId=${id}`);
+        if (res.code === 1) {
+          this.$message.success({ content: '删除成功', key: id, duration: 3 });
+          this.fetchTask();
+        } else {
+          this.$message.error({ content: '删除失败', key: id, duration: 3 });
+        }
+      } catch (err) {
+        this.$message.error({ content: '删除失败', key: id, duration: 3 });
+      }
+    },
+    async onClickExecuteTask(id) {
+      const res = await this.$axios(`/analysis/online/task/execute?taskId=${id}`);
+      if (res.code === 1) {
+        this.$notification.success({
+          message: '执行成功',
+          description: `${JSON.stringify(res.data.result, null, 2)}`,
+        });
+      } else {
+        this.$notification.error({
+          message: '执行失败',
+          description: res.msg,
+        });
+      }
+    },
     beforeModelUpload(file) {
       this.modelFile = [file];
       return false;
@@ -320,6 +535,9 @@ export default {
       );
       if (res.code === 1) {
         this.fetchModel();
+        this.addModelModalVisible = false;
+        this.modelForm = {};
+        this.modelFile = [];
       } else {
         this.$message.warning(res.msg, 3);
       }
@@ -338,12 +556,71 @@ export default {
         this.$message.warning(res.msg, 3);
       }
     },
+    async onSelectTaskModel(modelId) {
+      const res = await this.$axios(`/analysis/online/model/inputs?modelId=${modelId}`);
+      if (res.code === 1) {
+        this.taskModelInputs = res.data;
+        res.data.forEach((input) => {
+          this.inputForm[input.fieldId] = {
+            channelFieldIds: [],
+          };
+        });
+      } else {
+        this.$message.warning('请求模型输入字段出错!', 3);
+      }
+    },
+    async onSelectChannels(channels) {
+      let fieldList = [];
+      const requestList = [];
+      channels.forEach((channelId) => requestList.push(this.$axios(`/channel/get/data?channelType=1&channelId=${channelId}`)
+        .then((res) => {
+          fieldList = fieldList.concat(
+            Array.from(res).map((field) => ({
+              id: field.id,
+              fieldName: `ChannelId:${channelId}:${field.fieldName}`,
+            })),
+          );
+        })));
+      Promise.all(requestList).then(() => { this.fieldList = fieldList; });
+    },
     handleModelModalCancel() {
       this.addModelModalVisible = false;
+      this.modelForm = {};
+      this.modelFile = [];
     },
     handleOperatorModalCancel() {
       this.operatorModalVisible = false;
       this.operatorForm = {};
+    },
+    handleTaskModalCancel() {
+      this.taskModalVisible = false;
+      this.taskForm = {};
+      this.inputForm = {};
+    },
+    onInputFieldChannelFieldChange(value, inputFieldId) {
+      this.inputForm[inputFieldId].channelFieldIds = value;
+    },
+    onInputFieldOperatorChange(value, inputFieldId) {
+      this.inputForm[inputFieldId].operatorId = value;
+    },
+    async submitTask() {
+      const functions = Object.entries(this.inputForm).map(([inputFieldId, val]) => ({
+        channelFieldIds: val.channelFieldIds,
+        inputFieldId,
+        operatorId: val.operatorId,
+      }));
+      const res = await this.$axios.post(
+        '/analysis/online/task/create',
+        { ...this.taskForm, functions },
+      );
+      if (res.code === 1) {
+        this.fetchTask();
+        this.taskModalVisible = false;
+        this.taskForm = {};
+        this.inputForm = {};
+      } else {
+        this.$message.warning(res.msg, 3);
+      }
     },
   },
 };
